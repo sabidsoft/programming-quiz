d44ebcfcaef8645de3e8db75f48806b82e57b049
Sabid Hasan <sabid.soft@gmail.com>
2022-10-10 22:35:17 +0600

    tailwind and router setuped

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Error from './components/Error/Error';
+import Home from './components/Home/Home';
+import Main from './layouts.js/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main/>,
+      errorElement: <Error/>,
+      children: [
+        {
+          path: '/home',
+          element: <Home/>
+        },
+      ]
+    }
+  ])
+
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/components/Error/Error.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Error = () => {
+    const navigate = useNavigate()
+    return (
+        <div className='h-screen flex flex-col justify-center items-center'>
+            <h1 className='text-3xl font-semibold mb-5'>Opps!</h1>
+            <h1 className='text-5xl font-bold mb-5'>404</h1>
+            <h1 className='text-3xl font-semibold mb-5'>Page not Found!</h1>
+            <button onClick={() => navigate('/')} className='underline rounded'>Go to Home</button>
+        </div>
+    );
+};
+
+export default Error;
